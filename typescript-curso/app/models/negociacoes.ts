df272dd3adccdec40b5eac6bda1878e3b7042b07
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-05-18 21:40:41 -0300

    Programação defensiva

--- typescript-curso/app/models/negociacoes.ts
+++ typescript-curso/app/models/negociacoes.ts
@@ -3,11 +3,11 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
 
-    adiciona(negociacao: Negociacao) {
+    adiciona(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao);
     }
 
-    lista(): Array<Negociacao> {
+    lista(): ReadonlyArray<Negociacao> {
         return this.negociacoes;
     }
 }
