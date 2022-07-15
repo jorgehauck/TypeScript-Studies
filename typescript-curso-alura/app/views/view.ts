aa2eb5fced64afc6cc64edcd48aeefba61f99080
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-07-14 21:40:51 -0300

    Enumerations

--- typescript-curso-alura/app/views/view.ts
+++ typescript-curso-alura/app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
 
     protected elemento: HTMLElement;
 
@@ -6,12 +6,10 @@ export class View<T> {
         this.elemento = document.querySelector(seletor);
     }
 
-    update(model: T): void {
+    public update(model: T): void {
         const template = this.template(model);
         this.elemento.innerHTML = template;
     }
 
-    template(model: T): string {
-        throw Error("Classe filha precisa implementar a classe filha");
-    }
+    protected abstract template(model: T): string;
 }
--- typescript-curso-alura/dist/js/controllers/negociacao-controller.js
+++ typescript-curso-alura/dist/js/controllers/negociacao-controller.js
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from './../enums/dias-da-semana.js';
 import { MensagemView } from './../views/mensagem-view.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
@@ -7,6 +8,8 @@ export class NegociacaoController {
         this.negociacoes = new Negociacoes();
         this.negociacoesView = new NegociacoesView('#negociacoesView');
         this.mensagemView = new MensagemView('#mensagemView');
+        this.SABADO = 6;
+        this.DOMINGO = 0;
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
@@ -14,10 +17,13 @@ export class NegociacaoController {
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
+        if (!this.diaUtil(negociacao.data)) {
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
+            return;
+        }
         this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação adicionada com sucesso!');
         this.limparFormulario();
+        this.atualizaView();
     }
     criaNegociacao() {
         const exp = /-/g;
@@ -31,5 +37,12 @@ export class NegociacaoController {
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
+    }
+    diaUtil(data) {
+        return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
+    }
+    atualizaView() {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso!');
     }
 }
